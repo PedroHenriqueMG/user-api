1d0fd4d868cf0233691fc55e63505d97b0a7a5f4
PedroHenriqueMG <pedrohdefsouza@gmail.com>
2024-04-15 22:31:11 -0300

    fix: api connect to db

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,26 @@ import "express-async-errors";
 import Express from "express";
 import router from "./routes";
 import { errosMiddleware } from "./middleware/error";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const app = Express();
 app.use(Express.json());
 
 app.use(router);
+
+prisma
+  .$connect()
+  .then(() => {
+    console.log("Conexão com o banco de dados estabelecida com sucesso.");
+  })
+  .catch((error) => {
+    console.error("Erro ao conectar ao banco de dados:", error);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
 
 app.use(errosMiddleware);
 app.listen(process.env.PORT, () => {
